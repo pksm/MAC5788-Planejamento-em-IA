95e0bc2b01645b07702e84448c51f087e8d07d8d
pksm <paulamoraes95@gmail.com>
2017-09-13 01:10:12 -0300

    parei no grounding

--- blindsearch.py
+++ blindsearch.py
@@ -19,6 +19,8 @@ class blindSearch(object):
 		self._domain = domain
 		self._problem = problem
 		self._search = search
+		self.operatorsPrecond = {}
+		self.genDict()
 		#self._objects[obj.type] = self._objects.get(obj.type, [])
 		# self.domainOperators = {}
 		# for i in self.domain.operators:
@@ -38,6 +40,16 @@ class blindSearch(object):
 	def search(self):
 		return self._search
 
+	def genDict(self):
+		all_actions = self._domain.operators
+		for a in all_actions:
+			for p in a.precond:
+				if p.predicate.name != '=':
+					self.operatorsPrecond[a] = self.operatorsPrecond.get(a, set()) #na hora de fazer union, intersect, difference mudar para set
+					self.operatorsPrecond[a].add(re.sub(r'\([^)]*\)', '', str(p)))
+					# self.operatorsPrecond[a.name] = self.operatorsPrecond.get(a.name, set()) #na hora de fazer union, intersect, difference mudar para set
+					# self.operatorsPrecond[a.name].add(re.sub(r'\([^)]*\)', '', str(p)))
+
 	# @property
 	# def domainOperators(self):
 	# 	return self._domainOperators.copy()
@@ -49,13 +61,28 @@ class blindSearch(object):
 		return ((self.problem.goal & state) == self.problem.goal) #goal test
 		#before last update: return ((self.problem._goal & state) == self.problem._goal)
 
-	def applicableActions(self,state):
-		validActions = []
+	def grounding(self, actions, literals,types): ??
+
+
+	def applicableActions(self,state): #esperando um state do tipo set
+		validActions = set()
 		all_actions = self.domain.operators
 		literals = self.problem.objects
-		types = list(literals.keys()) #pode ser alterado para set 
-		types2 = list(literals.items())
-		precondAct = []
+		atomState = {re.sub(r'\([^)]*\)', '', str(i)) for i in self.problem.init} #sera state depois
+		for i in self.operatorsPrecond.keys():
+			if ((self.operatorsPrecond[i] & atomState) == self.operatorsPrecond[i]):
+				validActions.add(i.name)
+		print ("VALID FOR > ", self.problem.init, "ACTIONS > ", validActions )
+
+
+
+
+
+		# atomState = re.sub(r'\([^)]*\)', '', str(self.problem.init))
+		#print(atomState, type(self.problem.init), self.problem.init)
+		#types = list(literals.keys()) #pode ser alterado para set 
+		#types2 = list(literals.items())
+		#precondAct = []
 		'''
 		Ideia:
 			criar dois dicts: (1) dict predicate (2) dict equality (pode ser um set com o nome das actions que contenham tipo equality na precond)
@@ -63,22 +90,18 @@ class blindSearch(object):
 
 			se tem equality nas preconds, entao deve ter uma flag para detectar isso e testar os literais
 
-			NOW:
+			OLD:
 			- pegar toda letra que sucede ? em Action.params e adicionar a um set para nao ter duplicadas
 			- para cada letra gerar um novo set com o mesmo index e atribuir 
+			------------------------------------
+			NOW: a ideia eh q dado um set de atomos, eh possivel verificar a aplicabilidade dos operadores disponiveis sem instanciar, olhar num 
+			dicionario talvez...chamar funcao q cria esse dicionario assim que entra no __init___
+				[action.name] = [action.precond (sem os predicados de igualdade)]	DICT referente a DOMAIN
+
 		'''
-		for i in range(len(all_actions)):
-			#precondAct.append()
-			# print (all_actions[i].name)
-			# print (all_actions[i].params[0].type)
-			for j in range(len(all_actions[i].precond)):
-				print (all_actions[i].precond[j].predicate.name) 
-
-
 		for a in all_actions:
 			if ((set(a.precond) & state) == set(a.precond)):
 				validActions.append(a.name)
 
-		return types2
-
+		return 
 
